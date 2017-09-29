852606aede7044150abf7490dc55a9a651f92806
Thomas Lenz <thomas.lenz96@gmail.come>
2017-09-28 21:37:14 -0500

    Removed the azure-data folder Implemented PHP for the banner

---
+++ projects/p-02-first-website/labs.php
@@ -0,0 +1,37 @@
+<!--
+	@author Thomas Lenz <thomas.lenz96@gmail.com> AS The2b
+	@date 28 Sept 2017
+	@project Azure Website
+	@file labs.php
+ 	@course Web and Distributed Programming
+	@semester Fall 2017
+-->
+
+<!DOCTYPE=html>
+<html>
+	<head>
+		<!-- Source CSS -->
+		<link rel="stylesheet" type="text/css" href="./azure-website.css"></link>
+		<meta charset="utf-8" />
+		<!-- <title>Web and Distributed Programming Assignment Archive</title> -->
+		<title>WaDPAA</title>
+	</head>
+
+	<!-- Stops the margins from breaking the top navbar -->
+	<body class="bodyStyle">
+
+		<?php
+			include('header.html');
+       		?>
+
+		<p class="defaultText">
+			<h1>Labs</h1>
+			<hr />
+			Lab 1: Hello World <a href="https://github.com/The2b/Fall-2017-LewisU-Web-And-Dist-Programming/tree/master/labs/l-01-hello-world">[GitHub]</a> <a href="https://github.com/The2b/Fall-2017-LewisU-Web-And-Dist-Programming/raw/master/labs/l-01-hello-world/l-01-hello-world.zip">[ZIP]</a><br />
+			Lab 2: Course Schedule <a href="https://github.com/The2b/Fall-2017-LewisU-Web-And-Dist-Programming/tree/master/labs/l-02-course-schedule">[GitHub]</a> <a href="https://github.com/The2b/Fall-2017-LewisU-Web-And-Dist-Programming/raw/master/labs/l-02-course-schedule/l-02-course-schedule.zip">[ZIP]</a><br />
+			Lab 3: Course Schedule 2 &mdash; Electric Boogaloo <a href="https://github.com/The2b/Fall-2017-LewisU-Web-And-Dist-Programming/tree/master/labs/l-03-course-schedule-css">[GitHub]</a> <a href="https://github.com/The2b/Fall-2017-LewisU-Web-And-Dist-Programming/raw/master/labs/l-03-course-schedule-css/l-03-course-schedule-css.zip">[ZIP]</a><br />
+			Lab 4: JavaScript Basics <a href="https://github.com/The2b/Fall-2017-LewisU-Web-And-Dist-Programming/tree/master/labs/l-04-javascript-basics">[GitHub]</a><br />
+		</p>
+	</body>
+
+</html>
---
+++ projects/p-02-first-website/projects.php
@@ -0,0 +1,35 @@
+<!--
+	@author Thomas Lenz <thomas.lenz96@gmail.com> AS The2b
+	@date 28 Sept 2017
+	@project Azure Website
+	@file projects.php
+ 	@course Web and Distributed Programming
+	@semester Fall 2017
+-->
+
+<!DOCTYPE=html>
+<html>
+	<head>
+		<!-- Source CSS -->
+		<link rel="stylesheet" type="text/css" href="./azure-website.css"></link>
+		<meta charset="utf-8" />
+		<!-- <title>Web and Distributed Programming Assignment Archive</title> -->
+		<title>WaDPAA</title>
+	</head>
+
+	<!-- Stops the margins from breaking the top navbar -->
+	<body class="bodyStyle">
+		<!-- Defines the Navbar -->
+		<?php
+			include('header.html');
+		?>
+
+		<p class="defaultText">
+			<h1>Projects</h1>
+			<hr />
+			Project 1: Favorite Hobby <a href="https://github.com/The2b/Fall-2017-LewisU-Web-And-Dist-Programming/tree/master/projects/p-01-favorite-hobby">[GitHub]</a> <a href="https://github.com/The2b/Fall-2017-LewisU-Web-And-Dist-Programming/raw/master/projects/p-01-favorite-hobby/p-01-favorite-hobby.zip">[ZIP]</a><br />
+			Project 2: Azure Website <a href="https://github.com/The2b/Fall-2017-LewisU-Web-And-Dist-Programming/tree/master/projects/p-02-first-website">[GitHub]</a><br />
+		</p>
+	</body>
+
+</html>
