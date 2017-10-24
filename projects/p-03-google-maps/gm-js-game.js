b58ddcacdc8da2e51ec1079c197b95fb6745938e
Thomas Lenz <thomas.lenz96@gmail.come>
2017-10-24 11:26:03 -0500

    98% Finished. Still need to add the following: game over, splash/loading screen, help text

--- projects/p-03-google-maps/gm-js-game.js
+++ projects/p-03-google-maps/gm-js-game.js
@@ -9,12 +9,21 @@
 
 var NUM_LOCATIONS = 8;
 var REQUIRED_ZOOM = 8;
+var DEFAULT_ZOOM = 5;
+var MARKER_ZOOM = 16;
+
+// Just so I don't forget what they are
+var FIVE_HUNDRED_MILES = 804672;
+var TWO_FIFTY_MILES = 402336;
+var ONE_HUNDRED_MILES = 160934;
+var FIFTY_MILES = 80467;
 
 var myMap;
 var locations;
 var locationNames;
 var locationHints;
 var startingPoints; // @TODO hard code starting points
+var markers;
 var locationIndex = 0;
 var score = 0;
 
@@ -30,12 +39,18 @@ function buildMap() {
 		zoom: 1
 	});
 
+	// Move the map
+	myMap.setCenter(startingPoints[0]);
+	myMap.setZoom(DEFAULT_ZOOM);
+	
+	// Start hybrid view mode
+	myMap.setMapTypeId(google.maps.MapTypeId.HYBRID);
+
 	// Attach events
 	attachListeners();
 
-	// Move the map
-	myMap.setCenter(startingPoints[0]);
-	myMap.setZoom(4);
+	// Show hints
+	document.getElementById("hint").display = "inline-block";
 }
 
 /**
@@ -45,8 +60,8 @@ function buildMap() {
 
 function resetMap() {
 	//myMap.setCenter(startingPoints[locationIndex]); @TODO @DEBUG
-	myMap.setCenter(startingPoints[0]);
-	myMap.setZoom(4);
+	myMap.setCenter(startingPoints[0]); // @DEBUG
+	myMap.setZoom(DEFAULT_ZOOM);
 }
 
 /**
@@ -59,12 +74,12 @@ function resetMap() {
 function checkWinConditions() {
 	//alert("Event works. Zoom level: " + myMap.getZoom()); // @DEBUG @TODO
 	if(myMap.getZoom() < REQUIRED_ZOOM) {
-		// @TODO giveHint(); // What hint to give is handled in this function
+		giveHint(); // What hint to give is handled in this function
 	}
 
 	else if(myMap.getZoom() >= REQUIRED_ZOOM) {
 		if(myMap.getBounds().contains(locations[locationIndex])) {
-			alert ("A Winner is you!!");
+			//alert ("A Winner is you!!"); @DEBUG
 			win();
 		}
 	}
@@ -78,10 +93,11 @@ function checkWinConditions() {
 function win() {
 	if(locationIndex < NUM_LOCATIONS-1) {
 		alert("You found location number " + (locationIndex+1) + ", " + locationNames[locationIndex]);
+		markers[locationIndex].setMap(myMap);
 		locationIndex++;
-		score++;
+		score++; // @TODO markers
+		document.getElementById("score").innerHTML = ("Score: " + score);
 		resetMap();
-		// @TODO update footer
 	}
 
 	else {
@@ -125,29 +141,124 @@ function defineLocations() {
 	];
 	
 	startingPoints = [
-		new google.maps.LatLng(0.0,0.0) // @TODO @DEBUG
+		new google.maps.LatLng(40.979898,-97.207031) // @TODO Maybe change this. It's about middle America atm
+	];
+
+	markers = [
+		new google.maps.Marker({
+			position: locations[0],
+			title: locationNames[0],
+			label: "1"
+		}),
+		
+		new google.maps.Marker({
+			position: locations[1],
+			title: locationNames[1],
+			label: "2"
+		}),
+		
+		new google.maps.Marker({
+			position: locations[2],
+			title: locationNames[2],
+			label: "3"
+		}),
+		
+		new google.maps.Marker({
+			position: locations[3],
+			title: locationNames[3],
+			label: "4"
+		}),
+		
+		new google.maps.Marker({
+			position: locations[4],
+			title: locationNames[4],
+			label: "5"
+		}),
+		
+		new google.maps.Marker({
+			position: locations[5],
+			title: locationNames[5],
+			label: "6"
+		}),
+		
+		new google.maps.Marker({
+			position: locations[6],
+			title: locationNames[6],
+			label: "7"
+		}),
+		
+		new google.maps.Marker({
+			position: locations[7],
+			title: locationNames[7],
+			label: "8"
+		})
 	];
 }
 
+// @TODO Probably should delete this @NOTE
 function giveHint() { // @TODO
-	if(myMap.getBounds().contains(locations[locationIndex])) {
-		// @TODO
-		// Give hint, update footer
+	document.getElementById("hint").innerHTML = hintFooter();
+	document.getElementById("hint").style.visibility = "visible";
+	return;
+}
+
+function helpMessage() { // @TODO
+	alert(
+		""
+	);
+}
+
+function hintPopup() {
+	alert(locationHints[locationIndex]);
+}
+
+function hintFooter() {
+	var distance = google.maps.geometry.spherical.computeDistanceBetween(myMap.getCenter(),locations[locationIndex]);
+	var hint = "";
+	if(distance < FIFTY_MILES) {
+		hint = "White Hot";
+		return hint;
+	}
+	
+	else if(distance < ONE_HUNDRED_MILES) {
+		hint = "On Fire!";
+		return hint;
+	}
+
+	else if(distance < TWO_FIFTY_MILES) {
+		hint = "Cold";
+		return hint;
+	}
+
+	else if(distance < FIVE_HUNDRED_MILES) {
+		hint = "May as well be in Antarctica";
+		return hint;
+	}
+
+	else if(distance > FIVE_HUNDRED_MILES) {
+		hint = "Absolute zero";
+		return hint;
 	}
 
 	else {
-		// @TODO
+		hint = "Something's wrong. Distance is NULL.";
+		return hint;
 	}
 }
 
-function gettingStarted() { // @TODO
-
-}
-
-function attachListeners() { // @TODO
+function attachListeners() {
 	myMap.addListener('bounds_changed', checkWinConditions);
-}
+	document.getElementById("gettingStarted").addEventListener("click",helpMessage);
+	document.getElementById("hint").addEventListener("click",hintPopup);
 
-function buildFooter() { // @TODO
-
+	// This is the simplest way I could do it without passing vars through like the instructor said not to do in class
+	for(var index = 0;index < NUM_LOCATIONS; index++) {
+		markers[index].setClickable(true);
+		let subIndex = index;
+		markers[index].addListener("dblclick", function() { 
+			var index = subIndex;
+			myMap.setCenter(markers[index].getPosition());
+			myMap.setZoom(MARKER_ZOOM);
+		});
+	}
 }
